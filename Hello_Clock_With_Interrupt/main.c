d9f7050ca8def0cc69623640f91bf3b040881c39
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-01-02 22:06:31 +0000

    Update main.c

--- Hello_Clock_With_Interrupt/main.c
+++ Hello_Clock_With_Interrupt/main.c
@@ -11,169 +11,108 @@
  *
  */
 
-#include <msp430.h> 
+#include <msp430.h>
+#include <serialprint.h>
+#include <manipulate_clocks.h>
+#include <manipulate_leds.h>
 
-#define SW BIT3
-#define LEDR BIT6
-#define LEDG BIT0
+
 int clock_mode = 0;
 
-// LFXT1: 0.4s-1MHz
-void register_settings_for_VLO()
-{
-   volatile unsigned long i;
-   BCSCTL3 |= LFXT1S_2;                 // LFXT1 = VLO
-   do{
-       IFG1 &= ~OFIFG;                  // Clear oscillator fault flag
-       for (i = 50000; i; i--);         // Delay
-     } while (IFG1 & OFIFG);            // Test OSC fault flag
-   BCSCTL2 |= SELM_3;                   // MCLK = VLOCLK
+void setup(){
+    // direction led's
+    P1DIR |= BIT6;      // set red led pin as output
+    P1DIR |= BIT0;      // set green led pin as output
+
+    // direction RGB
+    P2DIR |= BIT1;      // configure P2.1 as output R
+    P2DIR |= BIT3;      // configure P2.3 as output G
+    P2DIR |= BIT5;      // configure P2.5 as output B
+
+    // direction switch
+    P1DIR &= ~BIT3;     // set switch pin as input
+
+    // startup states RGB
+    P1OUT &=~ (BIT8 + BIT6);    // clear P2.1  (RGB red off)
+    P1OUT &=~ (BIT8 + BIT0);    // clear P2.1  (RGB red off)
+
+    // startup states switch
+    P1OUT |= BIT3;              // select pull up for switch pin
+    P1REN |= BIT3;              // enable resistor for switch pin
+    P1IES &= ~BIT3;             // select interrupt on rising edge
+    P1IE |= BIT3;               // enable interrupt on switch pin
 }
 
 int main(void)
 {
-    WDTCTL = WDTPW | WDTHOLD;   // stop Watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;           // stop Watchdog timer
 
-    volatile unsigned long a;
-    for(a = 0; a<20000; a++);        // startup delay to allow clocks to initialise
+    setup();                            // initial setup
 
-    // setup for serial transmission
-    UARTConfigure();
-
-    // setup LEDs
-    P1DIR |= LEDR;      // set led pin as output
-    P1DIR |= LEDG;      // set led pin as output
-
-    // setup RGB
-    P2DIR |= BIT1;                  // configure P2.1 as output R
-    P2DIR |= BIT3;                  // configure P2.3 as output G
-    P2DIR |= BIT5;                  // configure P2.5 as output B
-    P2OUT &=~ (BIT8 + BIT1);        // clear P2.1  (RGB red off)
-    P2OUT &=~ (BIT8 + BIT3);        // clear P2.3  (RGB green off)
-    P2OUT &=~ (BIT8 + BIT5);        // clear P2.5  (RGB blue off)
-
-    // setup switch
-    P1DIR &= ~SW;       // set switch pin as input
-    P1REN |= SW;        // enable resistor for switch pin
-    P1OUT |= SW;        // select pull up for switch pin
-    P1IES &= ~SW;       // select interrupt on rising edge
-    P1IE |= SW;         // enable interrupt on switch pin
+    UARTConfigure();                    // setup UART RXD & TXD
 
     __bis_SR_register(GIE);             // enable CPU interrupt
 
+    register_settings_for_VLO();        // initial VLO setup
+    MCLK_VLO_1_5kHz();                  // setup initial master clock configuration (low power mode)
+    RGB_0_RED();
+
     volatile unsigned long n;
-    register_settings_for_VLO();        //Register settings for VLO
-
-    // set initial clock frequency and display clock frequency mode using RGB LED
-    UARTSendArray("-- master clock signal using VLO as clock source at frequency: 1.5kHz (low power mode)", 86);
-    UARTSendArray("\r\n", 2);
-    P2OUT ^= BIT1;                  // toggle P2.1 (RGB red on: displays clock mode 0)
-    P2OUT &=~ (BIT8 + BIT3);        // clear P2.3  (RGB green off)
-    P2OUT &=~ (BIT8 + BIT5);        // clear P2.5  (RGB blue off)
-    BCSCTL2 &=~ (BIT5 + BIT4);      // reset VLO divider
-    BCSCTL2 |= (BIT5 + BIT4);       // VLO = 12kHz/8 = 1.5kHz
-    clock_mode = 1;                 // iterate clock mode
-
     while(1) {
-        P1OUT ^= LEDG;                  // toggle led at approximately delay speed
+        P1OUT ^= BIT0;                  // toggle green led to broadly convey current master clock frequency
         for (n = 100; n > 0; n--);      // very short delay
     }
-
 }
 
 #pragma vector=PORT1_VECTOR
 __interrupt void Port_1(void)
 {
-    // display clock frequency mode conveyed in ascending colour frequency (red->green->blue)
     if(clock_mode==0)
     {
-        // delay from cycling back from high power
-        volatile unsigned long x;
-        for (x = 50000; x; x--);
-
-        // set display
-        P2OUT &=~ (BIT8 + BIT1);        // clear P2.1  (RGB red off)
-        P2OUT &=~ (BIT8 + BIT3);        // clear P2.3  (RGB green off)
-        P2OUT &=~ (BIT8 + BIT5);        // clear P2.3  (RGB green off)
-        P2OUT ^= BIT1;                  // toggle P2.1 (RGB red on: displays clock mode 0)
-
-        // switch back to VLO clock source for master clock and select LFXT1S clock speed range
-        DCOCTL = CALDCO_1MHZ;           // calibrate DCOCLK frequency
-        BCSCTL3 |= LFXT1S_2;            // LFXT1 = VLOLCK 0.4-1MHz
-        BCSCTL2 |= SELM_3;              // switch to VLOLCK as clock source for MCLK
-        BCSCTL2 &=~ (BIT5 + BIT4);      // reset MCLK Divider
-        BCSCTL2 |= (BIT5 + BIT4);       // MCLK Divider = 12kHz/8 = 1.5kHz
-
-        // transmit debug message over serial
+        MCLK_VLO_1_5kHz();
         UARTSendArray("\r\n", 2);
         UARTSendArray("-- master clock signal using VLO as clock source at frequency: 1.5kHz (low power mode)", 86);
         UARTSendArray("\r\n", 2);
-
-        clock_mode = 1;                 // iterate clock mode
+        RGB_0_RED();
     }
     else
     {
         if(clock_mode==1)
         {
-            UARTSendArray("-- master clock signal using VLO as clock source at frequency: 3kHz", 69);
-            UARTSendArray("\r\n", 2);                  // MCLK = VLOCLK
-            P2OUT &=~ (BIT8 + BIT1);        // clear P2.1  (RGB red off)
-            P2OUT &=~ (BIT8 + BIT3);        // clear P2.3  (RGB green off)
-            P2OUT &=~ (BIT8 + BIT5);        // clear P2.3  (RGB green off)
-            P2OUT ^= BIT3;                  // toggle P2.3 (RGB green: displays clock mode 1)
-            BCSCTL2 &=~ (BIT5 + BIT4);      // reset MCLK Divider
-            BCSCTL2 |= (BIT4);              // MCLK Divider = 12kHz/4 = 3kHz
-            clock_mode = 2;                 // iterate clock mode
+            MCLK_VLO_3kHz();
+            UARTSendArray("-- master clock signal using VLO as clock source at frequency: 3kHz", 68);
+            UARTSendArray("\r\n", 2);
+            RGB_0_GREEN();
         }
         else
         {
             if(clock_mode==2)
             {
+                MCLK_VLO_12kHz();
                 UARTSendArray("-- master clock signal using VLO as clock source at frequency: 12kHz", 69);
                 UARTSendArray("\r\n", 2);
-                P2OUT &=~ (BIT8 + BIT1);        // clear P2.1  (RGB red off)
-                P2OUT &=~ (BIT8 + BIT3);        // clear P2.3  (RGB green off)
-                P2OUT &=~ (BIT8 + BIT5);        // clear P2.3  (RGB green off)
-                P2OUT ^= BIT5;                  // toggle P2.5 (RGB blue: displays clock mode 2)
-                BCSCTL2 &=~ (BIT5 + BIT4);      // MCLK Divider = 12kHz/1 = 12kHz
-                clock_mode = 3;                 // reset clock mode
+                RGB_0_BLUE();
             }
             else
             {
                 if(clock_mode==3)
                 {
+                    MCLK_DCO_125kHz();
                     UARTSendArray("-- master clock signal using DCO as clock source at frequency: 125kHz", 69);
                     UARTSendArray("\r\n", 2);
-                    P2OUT &=~ (BIT8 + BIT1);        // clear P2.1  (RGB red off)
-                    P2OUT &=~ (BIT8 + BIT3);        // clear P2.3  (RGB green off)
-                    P2OUT &=~ (BIT8 + BIT5);        // clear P2.3  (RGB green off)
-                    P2OUT ^= BIT1;                  // toggle P2.1 (RGB red on: displays clock mode 0)
-                    P2OUT ^= BIT5;                  // toggle P2.5 (RGB blue: displays clock mode 2)
-                    DCOCTL = CALDCO_1MHZ;           // calibrate DCOCLK frequency
-                    BCSCTL2 = SELM0;                // switch to DCOCLK as clock source for MCLK
-                    BCSCTL2 &=~ (BIT5 + BIT4);      // reset MCLK divider
-                    BCSCTL2 |= (BIT5 + BIT4);       // MCLK Divider = 1MHz/8 = 125kHz
-                    clock_mode = 4;                 // reset clock mode
+                    RGB_0_PURPLE();
                 }
                 else
                 {
                     if(clock_mode==4)
                     {
                         UARTSendArray("-- master clock signal using DCO as clock source at frequency: 16MHz (high power mode)", 88);
-                        P2OUT &=~ (BIT8 + BIT1);        // clear P2.1  (RGB red off)
-                        P2OUT &=~ (BIT8 + BIT3);        // clear P2.3  (RGB green off)
-                        P2OUT &=~ (BIT8 + BIT5);        // clear P2.3  (RGB green off)
-                        P2OUT ^= BIT1;                  // toggle P2.1 (RGB red on: displays clock mode 0)
-                        P2OUT ^= BIT3;                  // toggle P2.3 (RGB green: displays clock mode 1)
-                        P2OUT ^= BIT5;                  // toggle P2.5 (RGB blue: displays clock mode 2)
-                        DCOCTL = CALDCO_16MHZ;           // calibrate DCOCLK frequency
-                        BCSCTL2 = SELM0;                // switch to DCOCLK as clock source for MCLK
-                        BCSCTL2 &=~ (BIT5 + BIT4);      // reset MCLK divider
-                        clock_mode = 0;                 // reset clock mode
+                        RGB_0_WHITE();
+                        MCLK_DCO_16MHz();
                     }
                 }
             }
         }
     }
-    P1IFG &= ~SW;    // clear switch interrupt flag
+    P1IFG &= ~BIT3;    // clear switch interrupt flag
 }
