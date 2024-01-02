68b33358f803b3d1ae46db6d3a507dafc555dfdc
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-01-02 22:19:08 +0000

    Update serialprint.h

--- Hello_Clock_With_Interrupt/serialprint.h
+++ Hello_Clock_With_Interrupt/serialprint.h
@@ -15,20 +15,20 @@ volatile char UARTRxFlag = 0;
 volatile char i=0;
 
 void UARTConfigure(void){
-    P1SEL = BIT1 + BIT2 ;       // P1.1 = RXD, P1.2=TXD
-    P1SEL2 = BIT1 + BIT2 ;      // P1.1 = RXD, P1.2=TXD
-    UCA0CTL1 |= UCSSEL_2;       // Use SMCLK
-    UCA0BR0 = 104;              // Set baud rate to 9600 with 1MHz clock (Data Sheet 15.3.13)
-    UCA0BR1 = 0;                // Set baud rate to 9600 with 1MHz clock
-    UCA0MCTL = UCBRS0;          // Modulation UCBRSx = 1
-    UCA0CTL1 &= ~UCSWRST;       // Initialise USCI state machine
-    IE2 |= UCA0RXIE;            // Enable USCI_A0 RX interrupt
+    P1SEL = BIT1 + BIT2 ;               // P1.1 = RXD, P1.2=TXD
+    P1SEL2 = BIT1 + BIT2 ;              // P1.1 = RXD, P1.2=TXD
+    UCA0CTL1 |= UCSSEL_2;               // Use SMCLK
+    UCA0BR0 = 104;                      // Set baud rate to 9600 with 1MHz clock (Data Sheet 15.3.13)
+    UCA0BR1 = 0;                        // Set baud rate to 9600 with 1MHz clock
+    UCA0MCTL = UCBRS0;                  // Modulation UCBRSx = 1
+    UCA0CTL1 &= ~UCSWRST;               // Initialise USCI state machine
+    IE2 |= UCA0RXIE;                    // Enable USCI_A0 RX interrupt
 }
 
 void UARTSendChar(unsigned char character)
 {
-    while(!(IFG2 & UCA0TXIFG));     // Wait for TX buffer to be ready for new data
-    UCA0TXBUF = character;          //Write the character at the location specified by the pointer and increment pointer
+    while(!(IFG2 & UCA0TXIFG));         // Wait for TX buffer to be ready for new data
+    UCA0TXBUF = character;              //Write the character at the location specified by the pointer and increment pointer
 }
 
 void UARTSendArray(unsigned char *TxArray, unsigned char ArrayLength)
@@ -52,11 +52,11 @@ void UARTSendString(char *TxArray)
 #pragma vector=USCIAB0RX_VECTOR
 __interrupt void USCI0RX_ISR(void)
 {
-    UARTRxData[i] = UCA0RXBUF;      //Read USART data register
+    UARTRxData[i] = UCA0RXBUF;          //Read USART data register
     if(UARTRxData[i++]=='\r')
     {
-        UARTRxFlag=--i;             //Set String received flag to length of received string
-        i=0;                        //Reset buffer index
+        UARTRxFlag=--i;                 //Set String received flag to length of received string
+        i=0;                            //Reset buffer index
         __bic_SR_register_on_exit(CPUOFF);
     }
 }
