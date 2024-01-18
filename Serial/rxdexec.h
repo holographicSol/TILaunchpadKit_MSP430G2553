d3b6898f00db4eb3c996c60dc555d103bf440cd8
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-01-18 05:23:44 +0000

    Update rxdexec.h

--- Serial/rxdexec.h
+++ Serial/rxdexec.h
@@ -5,7 +5,7 @@ volatile char *com0 = "EXEC0";
 void RXD_Exec(char *received_chars)
 {
     if(memcmp(com0, received_chars, strlen(received_chars)+1) == 0)
-    {printtxd("[MSP_RXD] Running command EXEC0\n");}
+    {printtxd("[MSP] Running command EXEC0\n");}
 
-    else{printtxd("[MSP_RXD] Received unknown command\n");}
+    else{printtxd("[MSP] Received unknown command\n");}
 };
