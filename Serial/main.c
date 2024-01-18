b0c88de3ee986f0b53dcb94c7fbab5964b373221
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-01-18 06:51:17 +0000

    Update main.c

--- Serial/main.c
+++ Serial/main.c
@@ -6,14 +6,12 @@ void main(void)
     WDTCTL = WDTPW | WDTHOLD;  // stop WDT timer
     UARTConfigure();           // setup serial communication
     P1DIR |= BIT0;             // configure P1.0 as output (Green LED)
-
+    P1OUT |= BIT0;
     __bis_SR_register(GIE);    //  Enter LPM0, interrupts  enabled
 
     volatile unsigned int i;   // volatile to prevent optimisation
     printtxd("\n[MSP] Serial communication.\n");
     while(1)
     {
-        P1OUT ^= BIT0;
-        __delay_cycles(1);
     }
 }
