41a763fa9eab008311293e362207d676779e769d
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-01-19 04:10:38 +0000

    Update main.c

--- ADC_Interrupt/main.c
+++ ADC_Interrupt/main.c
@@ -6,11 +6,11 @@
 #include <uart.h>
 
 volatile unsigned  IDXBUFFADC10MEM  = 0;    // iterating ADC10MEM buffer index
-int                VOLTADC10MEM     = 0;    // hard voltage read from ADC10MEM
+float              VOLTADC10MEM     = 0;    // hard voltage read from ADC10MEM
 float              SVOLTADC10MEM    = 3.3;  // soft voltage for equations with ADC10MEM voltage
-char               STRADC10MEM[5];          // string type of ADC10MEM
-char               STRVOLTADC10MEM[5];      // string type of ADC10MEM converted to voltage
-char               STRRMDRADC10MEM[5];      // string type of ADC10MEM remainder from voltage conversion
+char               STRADC10MEM[16];          // string type of ADC10MEM
+char               STRVOLTADC10MEM[16];      // string type of ADC10MEM converted to voltage
+char               STRRMDRADC10MEM[16];      // string type of ADC10MEM remainder from voltage conversion
 
 void VOLTAGESTABALIZATIONADC10MEM(){P1REN |= BIT4; P1OUT &= ~BIT4;}  // optionally enable resistor pull-down (reduce flicker when no sensor)
 
@@ -18,14 +18,15 @@ void main(void)
 {
     WDTCTL = WDTPW + WDTHOLD;                                           // Stop WDT
     UARTConfigure();
-    ADC10CTL0 = SREF_0 + ADC10SHT_2 + REFON + ADC10ON + ADC10IE + MSC;  // Set ADC module
-    ADC10CTL1 = CONSEQ_0 + INCH_4 + ADC10DIV_0;                         // Set ADC module
-    ADC10DTC0 = ADC10CT;                                                // Set ADC module
+    ADC10CTL0 = SREF_0 + ADC10SHT_2 + REFON + ADC10ON + ADC10IE + MSC;  // SREF_0=V_CC and V_SS, ADC10SHT_2=sample-and-hold time to 16 x ADC10CLKs, REFON=internal reference voltage, ADC10IE=enables ADC10 interrupt, MSC=multiple sample & conversion
+    ADC10CTL1 = CONSEQ_0 + INCH_4 + ADC10DIV_0;                         // CONSEQ_0=single-channel, single-conversion, INCH_4=input channel A4 for ADC conversion, ADC10DIV_0=ADC10 clock divider to /1.
+    ADC10DTC0 = ADC10CT;                                                // ADC10CT=continuous transfer mode
     ADC10AE0 = BIT4;                                                    // P1.4 ADC option select
     ADC10CTL0 |= ENC + ADC10SC;                                         // Start Sampling and conversion
     __bis_SR_register(GIE);                                             // Enable Interrupts
     while(1){
-        ADC10CTL0 |= ADC10SC;
+        ADC10CTL0 |= ADC10SC;                                           // Start conversion
+        __bis_SR_register(CPUOFF + GIE);
     }
 }
 
@@ -33,16 +34,18 @@ void main(void)
 __interrupt void ADC10_IRS(void)
 {
     while (ADC10CTL1 & ADC10BUSY);
-    if(IDXBUFFADC10MEM < 16){
+    if(IDXBUFFADC10MEM < 20){
         IDXBUFFADC10MEM++;
     }
     else{
         VOLTADC10MEM = ((ADC10MEM*SVOLTADC10MEM)/1023);
+
         int VINTADC10MEM = floor(VOLTADC10MEM);
-        int RMDRADCMEM10 = floor((VOLTADC10MEM-VINTADC10MEM)*1000);
+        double RMDRADCMEM10 = floor((ADC10MEM-VINTADC10MEM)*1000);
         ltoa(ADC10MEM, STRADC10MEM);
         ltoa(VINTADC10MEM,STRVOLTADC10MEM);
-        ltoa(RMDRADCMEM10,STRRMDRADC10MEM);
+        ltoa((long)RMDRADCMEM10,STRRMDRADC10MEM);
+
         printtxd("\n[MSP ADC10MEM] "); printtxd(STRADC10MEM); printtxd("\n");
         printtxd("[MSP ADC10MEM V] "); printtxd(STRVOLTADC10MEM); printtxd("."); printtxd(STRRMDRADC10MEM); printtxd("\n");
         VOLTADC10MEM = 0;
@@ -52,4 +55,5 @@ __interrupt void ADC10_IRS(void)
         IDXBUFFADC10MEM = 0;
     }
     ADC10CTL0 &= ~(ADC10IFG);
+    __bic_SR_register_on_exit(CPUOFF);
 }
