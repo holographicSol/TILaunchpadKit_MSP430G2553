b8e4cb0477a3ff4e093d4d40cf55c42b7233e909
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-02-08 23:43:41 +0000

    Update main.c

--- Joystick_Analyzer/main.c
+++ Joystick_Analyzer/main.c
@@ -74,7 +74,6 @@ void joystick_0(void){
     ax = adc[5];
     ay = adc[4];
     ac = adc[3];
-
     // Zero to 500 converter: converts two analogue values to four 0-500 numbers.
     // An offset of 10 is also applied that accounts for a fluctuation range of 10 when my particular joy stick is idle (x=500-510, y=535-545). (A software patch for hardware problem).
     if       ((ay  <=  535) && (ax  <=  500))  {x0=abs(500-ax);  x1=0;               y0=abs((500-ay));  y1=0;               ssd1306_printText(0, 7, "[ULEFT] ");}
@@ -86,6 +85,17 @@ void joystick_0(void){
     else if  (ay  <=   535)                    {x0=0;            x1=0;               y0=abs(500-ay);    y1=0;               ssd1306_printText(0, 7, "[UP]    ");}
     else if  (ay  >=   545)                    {x0=0;            x1=0;               y0=0;              y1=abs((ay/2)-10);  ssd1306_printText(0, 7, "[DOWN]  ");}
     else                                       {x0=0;            x1=0;               y0=0;              y1=0;               ssd1306_printText(0, 7, "[NONE]  ");}
+    // sanitise the normalisation (again joy stick specific, may not be necessary depending on quality of joy stick). this does not report corrections, only corrects:
+    if (x0 > 500 ){x0=500;}  // ensure 500 max
+    if (y0 > 500 ){y0=500;}  // ensure 500 max
+    if (x1 > 500 ){x1=500;}  // ensure 500 max
+    if (y1 > 500 ){y1=500;}  // ensure 500 max
+    if (x0 < 0 ){x0=0;}      // ensure   0 minimum
+    if (y0 < 0 ){y0=0;}      // ensure   0 minimum
+    if (x1 < 0 ){x1=0;}      // ensure   0 minimum
+    if (y1 < 0 ){y1=0;}      // ensure   0 minimum
+    if (y0 >= 496){y0=500;}  // in my case y goes no lower than 4 on analogue value so i will force 500 at from 496
+
     if       (ac  ==     0)                    {c0=1;}
     else                                       {c0=0;}
 }
