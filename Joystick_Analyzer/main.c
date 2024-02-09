51fd8e21b5f4769162154ea83a99bc4669752546
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-02-09 01:22:12 +0000

    Update main.c

--- Joystick_Analyzer/main.c
+++ Joystick_Analyzer/main.c
@@ -2,7 +2,6 @@
  * Joystick Analyzer: This setup increases/decreases values from approximately 0 to 500 for each direction.
  * All calls to ssd1306_printText can be removed outside of testing purposes, leaving only desired values being populated to remain.
  */
-
 #include <msp430.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -18,7 +17,6 @@ int x0 = 0;
 int x1 = 0;
 int y0 = 0;
 int y1 = 0;
-int c0 = 0;
 char x0_char[5];
 char x1_char[5];
 char y0_char[5];
@@ -32,32 +30,26 @@ unsigned int adc[8];
 void joystick_0(void);
 void main(void)
 {
-
     WDTCTL     = WDTPW | WDTHOLD;
-    i2c_init();
-    ssd1306_init();
-    ssd1306_clearDisplay();
-    ssd1306_printText(0, 1, "[ Joystick Analyzer ]");
     BCSCTL1    = CALBC1_1MHZ;
     DCOCTL     = CALDCO_1MHZ;
+    i2c_init();
+    ssd1306_init();
     ADC10CTL1  = INCH_7 + ADC10DIV_0 + CONSEQ_3 + SHS_0;
     ADC10CTL0  = SREF_0 + ADC10SHT_2 + MSC + ADC10ON;
     ADC10AE0   = BIT5 + BIT4 + BIT3 + BIT2 + BIT1 + BIT0;
     ADC10DTC1  = 8;
+    __delay_cycles(1000);
+    ssd1306_clearDisplay();
+    ssd1306_printText(0, 1, "[ Joystick Analyzer ]");
     while(1){
         ADC10CTL0 &= ~ENC;
         while (ADC10CTL1 & BUSY);
         ADC10CTL0 |= ENC + ADC10SC;
         ADC10SA = (unsigned int)adc;
-        ltoa(adc[5], vt_chara2);
-        ltoa(adc[4], vt_chara3);
-        ltoa(adc[3], vt_chara4);
+        ltoa(adc[5], vt_chara2); ltoa(adc[4], vt_chara3); ltoa(adc[3], vt_chara4);
         joystick_0();
-        ltoa(x0, x0_char);
-        ltoa(x1, x1_char);
-        ltoa(y0, y0_char);
-        ltoa(y1, y1_char);
-        ltoa(c0, c0_char);
+        ltoa(x0, x0_char); ltoa(x1, x1_char); ltoa(y0, y0_char); ltoa(y1, y1_char); ltoa(ac, c0_char);
         ssd1306_printText(0,  2, "X      ");  ssd1306_printText(15,  2, vt_chara2);
         ssd1306_printText(0,  3, "X0     ");  ssd1306_printText(15,  3, x0_char);
         ssd1306_printText(0,  4, "X1     ");  ssd1306_printText(15,  4, x1_char);
@@ -65,18 +57,16 @@ void main(void)
         ssd1306_printText(45, 3, "Y0     ");  ssd1306_printText(60,  3, y0_char);
         ssd1306_printText(45, 4, "Y1     ");  ssd1306_printText(60,  4, y1_char);
         ssd1306_printText(90, 2, "C     ");   ssd1306_printText(100, 2, vt_chara4);
-        ssd1306_printText(90, 3, "C0    ");   ssd1306_printText(105, 3, c0_char);
         }
 }
 
 void joystick_0(void){
-
     ax = adc[5];
     ay = adc[4];
     ac = adc[3];
     // Zero to 500 converter: converts two analogue values to four 0-500 numbers.
     // An offset of 10 is also applied that accounts for a fluctuation range of 10 when my particular joy stick is idle (x=500-510, y=535-545). (A software patch for hardware problem).
-    if       ((ay  <=  535) && (ax  <=  500))  {x0=abs(500-ax);  x1=0;               y0=abs((500-ay));  y1=0;               ssd1306_printText(0, 7, "[ULEFT] ");}
+    if       ((ay  <=  535) && (ax  <=  500))  {x0=abs(500-ax);  x1=0;               y0=abs(500-ay);    y1=0;               ssd1306_printText(0, 7, "[ULEFT] ");}
     else if  ((ay  <=  535) && (ax  >=  510))  {x0=0;            x1=abs((ax/2)-10);  y0=abs(500-ay);    y1=0;               ssd1306_printText(0, 7, "[URIGHT]");}
     else if  ((ay  >=  545) && (ax  <=  500))  {x0=abs(500-ax);  x1=0;               y0=0;              y1=abs((ay/2)-10);  ssd1306_printText(0, 7, "[DLEFT] ");}
     else if  ((ay  >=  545) && (ax  >=  510))  {x0=0;            x1=abs((ax/2)-10);  y0=0;              y1=abs((ay/2)-10);  ssd1306_printText(0, 7, "[DRIGHT]");}
@@ -94,8 +84,5 @@ void joystick_0(void){
     if (y0 < 0 ){y0=0;}      // ensure   0 minimum
     if (x1 < 0 ){x1=0;}      // ensure   0 minimum
     if (y1 < 0 ){y1=0;}      // ensure   0 minimum
-    if (y0 >= 496){y0=500;}  // in my case y goes no lower than 4 on analogue value so i will force 500 from 496
-
-    if       (ac  ==     0)                    {c0=1;}
-    else                                       {c0=0;}
+    if (y0 >= 496){y0=500;}  // in my case y goes no lower than 4 on analogue value so i will force 500 at from 496
 }
