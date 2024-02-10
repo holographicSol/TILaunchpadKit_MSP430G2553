37e48212b9f06bccf3dea86805dc3396575f3d00
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-02-10 06:15:14 +0000

    Update ajoystick.h

--- Joystick_Analyzer/ajoystick.h
+++ Joystick_Analyzer/ajoystick.h
@@ -18,7 +18,7 @@
 #include <string.h>
 #include <math.h>
 
-// ------------------------ calibration start: account for lower end joy sticks by providing calibration for stabilisation.
+// ------------------------ calibration: account for lower end joy sticks by providing calibration for stabilisation.
 int xyresolution;        // ensure resolution is approximately proportional to offset.  (high-end=512)
 int xidlemin;            // stabilise no x input using an x idle minimum.               (high-end=512)
 int xidlemax;            // stabilise no x input using an x idle minimum.               (high-end=512)
@@ -27,7 +27,6 @@ int yidlemax;            // stabilise no y input using an y idle max.           
 int xyminmaxstabalizer;  // stabilise x,y minimum and max input                         (high-end=0)
 int x_offset;            // idle range between x idle minimum and x idle maximum.       (high-end=0)    (sets automatically)
 int y_offset;            // idle range between y idle minimum and y idle maximum.       (high-end=0)    (sets automatically)
-// ------------------------ calibration end.
 
 // ---------------------------------- system place holders.
 int jx_ax                    = 0;  // raw analogue:   x axis
@@ -37,6 +36,10 @@ int jx_x0                    = 0;  // mapped left:    0-500
 int jx_x1                    = 0;  // mapped right:   0-500
 int jx_y0                    = 0;  // mapped up:      0-500
 int jx_y1                    = 0;  // mapped down:    0-500
+int jx_x2                    = 0;  // force:          left
+int jx_x3                    = 0;  // force:          right
+int jx_y2                    = 0;  // force:          up
+int jx_y3                    = 0;  // force:          down
 int jx_c0                    = 0;  // mapped clicked: zero/one
 
 // ---------------------------------- joy stick zero: copy/paste these place holders as jN to save more joy.
@@ -47,6 +50,10 @@ int j0_x0                    = 0;  // mapped left:    0-500
 int j0_x1                    = 0;  // mapped right:   0-500
 int j0_y0                    = 0;  // mapped up:      0-500
 int j0_y1                    = 0;  // mapped down:    0-500
+int j0_x2                    = 0;  // force:          left
+int j0_x3                    = 0;  // force:          right
+int j0_y2                    = 0;  // force:          up
+int j0_y3                    = 0;  // force:          down
 int j0_c0                    = 0;  // mapped clicked: zero/one
 
 void interpret_joy(int jx_ax, int jx_ay, int jx_ac, int xyresolution, int xidlemin, int xidlemax, int yidlemin, int yidlemax, int xyminmaxstabalizer, int joystickselect){
@@ -74,6 +81,23 @@ void interpret_joy(int jx_ax, int jx_ay, int jx_ac, int xyresolution, int xidlem
     if (jx_x1 <= (0+xyminmaxstabalizer))             {jx_x1=0;}             // sanitise minimum: 0 minimum
     if (jx_y1 <= (0+xyminmaxstabalizer))             {jx_y1=0;}             // sanitise minimum: 0 minimum
 
+    // set x,y force
+    if     ((jx_x0 > 0)  &&(jx_x0 <= 50 )){jx_x2=1;} else if((jx_x0 > 50) &&(jx_x0 <= 100)){jx_x2=1;} else if((jx_x0 > 100)&&(jx_x0 <= 150)){jx_x2=2;} else if((jx_x0 > 150)&&(jx_x0 <= 200)){jx_x2=3;}
+    else if((jx_x0 > 200)&&(jx_x0 <= 250)){jx_x2=4;} else if((jx_x0 > 250)&&(jx_x0 <= 300)){jx_x2=5;} else if((jx_x0 > 300)&&(jx_x0 <= 350)){jx_x2=6;} else if((jx_x0 > 350)&&(jx_x0 <= 400)){jx_x2=7;}
+    else if((jx_x0 > 400)&&(jx_x0 <= 450)){jx_x2=8;} else if((jx_x0 > 450)&&(jx_x0 <= 500)){jx_x2=8;} else if((jx_x0 > 500)&&(jx_x0 <= 550)){jx_x2=9;} else if(jx_x0==0){jx_x2=0;}
+
+    if     ((jx_x1 > 0)  &&(jx_x1 <= 50 )){jx_x3=1;} else if((jx_x1 > 50) &&(jx_x1 <= 100)){jx_x3=1;} else if((jx_x1 > 100)&&(jx_x1 <= 150)){jx_x3=2;} else if((jx_x1 > 150)&&(jx_x1 <= 200)){jx_x3=3;}
+    else if((jx_x1 > 200)&&(jx_x1 <= 250)){jx_x3=4;} else if((jx_x1 > 250)&&(jx_x1 <= 300)){jx_x3=5;} else if((jx_x1 > 300)&&(jx_x1 <= 350)){jx_x3=6;} else if((jx_x1 > 350)&&(jx_x1 <= 400)){jx_x3=7;}
+    else if((jx_x1 > 400)&&(jx_x1 <= 450)){jx_x3=8;} else if((jx_x1 > 450)&&(jx_x1 <= 500)){jx_x3=8;} else if((jx_x1 > 500)&&(jx_x1 <= 550)){jx_x3=9;} else if(jx_x1==0){jx_x3=0;}
+
+    if     ((jx_y0 > 0)  &&(jx_y0 <= 50 )){jx_y2=1;} else if((jx_y0 > 50) &&(jx_y0 <= 100)){jx_y2=1;} else if((jx_y0 > 100)&&(jx_y0 <= 150)){jx_y2=2;} else if((jx_y0 > 150)&&(jx_y0 <= 200)){jx_y2=3;}
+    else if((jx_y0 > 200)&&(jx_y0 <= 250)){jx_y2=4;} else if((jx_y0 > 250)&&(jx_y0 <= 300)){jx_y2=5;} else if((jx_y0 > 300)&&(jx_y0 <= 350)){jx_y2=6;} else if((jx_y0 > 350)&&(jx_y0 <= 400)){jx_y2=7;}
+    else if((jx_y0 > 400)&&(jx_y0 <= 450)){jx_y2=8;} else if((jx_y0 > 450)&&(jx_y0 <= 500)){jx_y2=8;} else if((jx_y0 > 500)&&(jx_y0 <= 550)){jx_y2=9;} else if (jx_y0==0){jx_y2=0;}
+
+    if     ((jx_y1 > 0)  &&(jx_y1 <= 50 )){jx_y3=1;} else if((jx_y1 > 50) &&(jx_y1 <= 100)){jx_y3=1;} else if((jx_y1 > 100)&&(jx_y1 <= 150)){jx_y3=2;} else if((jx_y1 > 150)&&(jx_y1 <= 200)){jx_y3=3;}
+    else if((jx_y1 > 200)&&(jx_y1 <= 250)){jx_y3=4;} else if((jx_y1 > 250)&&(jx_y1 <= 300)){jx_y3=5;} else if((jx_y1 > 300)&&(jx_y1 <= 350)){jx_y3=6;} else if((jx_y1 > 350)&&(jx_y1 <= 400)){jx_y3=7;}
+    else if((jx_y1 > 400)&&(jx_y1 <= 450)){jx_y3=8;} else if((jx_y1 > 450)&&(jx_y1 <= 500)){jx_y3=8;} else if((jx_y1 > 500)&&(jx_y1 <= 550)){jx_y3=9;} else if (jx_y1==0){jx_y3=0;}
+
     // ----- set values for joy stick selected: copy/paste this statement as jN and joy stick selected to save more joy.
-    if      (joystickselect==0){j0_x0=jx_x0; j0_x1=jx_x1; j0_y0=jx_y0; j0_y1=jx_y1; j0_c0=jx_c0;}
+    if      (joystickselect==0){j0_x0=jx_x0; j0_x1=jx_x1; j0_y0=jx_y0; j0_y1=jx_y1; j0_x2=jx_x2; j0_x3=jx_x3; j0_y2=jx_y2; j0_y3=jx_y3; j0_c0=jx_c0;}
 }
