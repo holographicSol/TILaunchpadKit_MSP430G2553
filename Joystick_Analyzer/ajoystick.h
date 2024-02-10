ab2204c4d06af6ddbac5ce19d0557236e003d287
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-02-10 00:38:19 +0000

    Update ajoystick.h

--- Joystick_Analyzer/ajoystick.h
+++ Joystick_Analyzer/ajoystick.h
@@ -10,7 +10,7 @@
  *                           (w) a single analogue joy stick.
  *                           (x) multiple analogue joy sticks, each overwriting stick zero place holder values.
  *                           (y) multiple analogue joy sticks using their own place holder values.
- *                           (z) 0-N input values that can be used as 'if not zero' and or as 'if input value between 0 and N'.
+ *                           (z) 0-N input values that can be used as 'if not zero' and or as 'if input value specifically in N range or equal to N'.
  */
 #include <stdlib.h>
 #include <stdio.h>
